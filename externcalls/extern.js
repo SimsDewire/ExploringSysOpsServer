e6fc5c610b6093ddc8b342de47d3041460e874d2
Lanselito <luab1100@student.miun.se>
2016-09-28 10:09:18 +0200

    Hanterar httpgetasync
    simpel metod för att hämta externa källor med http api

---
+++ externcalls/extern.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser')
+
+        function httpGetasync(theUrl, callback) {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() { 
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    callback(xmlHttp.responseText);
+                }
+            };
+    		xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    		xmlHttp.send(null);
+    	}
+
+    	function callback(data) {
+            console.log(data);
+        }
+
+router.fetchget(function (apicall,cb){
+	httpGetasync(apicall, function(data){
+		var tempholder = JSON.parse(data);
+	})
+
+});
+
+
