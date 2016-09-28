92201d78fcc5425ceb467179a0e7a5fa7b00abd0
paby1400 <paby1400@student.miun.se>
2016-09-28 11:24:11 +0200

    Add DB schemas for data history and plugins

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 // Connection URL
 var url = 'mongodb://localhost:27017/myproject';
 
+mongoose.Promise = global.Promise; // Remove promise warning
 mongoose.connect(url);
 
 var db = mongoose.connection;
@@ -11,3 +12,44 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback) {
 	console.log("Connection succeeded.");
 });
+
+// Load module DataHistory.js
+var DataHistoryModel = require('../models/DataHistory.js');
+
+// Create instance of DataHistory model to make testdata
+var testData = new DataHistoryModel();
+testData.type = "graph";
+testData.sender = "FK";
+testData.data = "saoid wqoie ihwqeowq hewqoie hwqewq";
+testData.time = new Date().toJSON().slice(0,10);
+
+// Save created testdata to database
+testData.save(
+	function(err) {
+		if (err) {
+			console.log("Could not store history testdata!");
+		} else {
+			console.log("Saved history testdata successfully: ");
+			console.log(testData);
+		}
+	}
+);
+
+// Load module Plugin.js
+var PluginModel = require('../models/Plugin.js');
+
+// Create instance of Plugin model
+var testDataPlugin = new PluginModel();
+testDataPlugin.pluginID = 500;
+testDataPlugin.source = "Something useful";
+
+testDataPlugin.save(
+	function(err) {
+		if (err) {
+			console.log("Could not store plugin testdata!");
+		} else {
+			console.log("Saved plugin testdata successfully: ");
+			console.log(testDataPlugin);
+		}
+	}
+);
