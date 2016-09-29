09f93e6eda3e6aaee7d5a7133ea5e745958ebb55
paby1400 <paby1400@student.miun.se>
2016-09-29 11:32:33 +0200

    Add all new collections and remove old collections

--- models/Plugin.js
+++ models/Plugin.js
@@ -1,12 +1,16 @@
 var mongoose = require("mongoose");
 
 var PluginSchema = new mongoose.Schema({
-	pluginID: Number,
-	source: String
+	name : String,
+	description : String,
+	creator : String,
+	version : String,
+	pluginPath : String,
+	webURL : String
 });
 
 // Force name of the collection to avoid standard naming
-PluginSchema.set('collection', 'Plugins');
+PluginSchema.set('collection', 'Plugin');
 
 var PluginModel = mongoose.model('Plugin', PluginSchema);
 
