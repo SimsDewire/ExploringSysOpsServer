8db34fbc3160345fc45faea87bf300d8ae1bcf19
paby1400 <paby1400@student.miun.se>
2016-11-17 12:11:15 +0100

    Fix mkdir -p problem to make it work for Windows as well

---
+++ mkdirp.js
@@ -0,0 +1,9 @@
+var mkdirp = require('mkdirp');
+    
+// Create folder logs/
+mkdirp('/logs', function (err) {
+    if (err) 
+    	console.error("Could not create logs/ (", err, ")");
+    else 
+    	console.log('Created folder logs/');
+});
